bcd61a3f74ef3313a505a818b509b23a85935a73
CeciliaCoelho <ceciliaeduarda@live.com.pt>
2022-12-05 13:11:34 +0000

    solutions to day 4

---
+++ day4.py
@@ -0,0 +1,18 @@
+f = open('input4.txt')
+
+data = f.read().strip().split('\n')
+
+data = list(map(lambda x: x.split(','), data))
+
+count = 0
+for i in data:
+    elf0 = i[0].split('-')
+    elf1 = i[1].split('-')
+
+    elf0 = [i for i in range(int(elf0[0]), int(elf0[1])+1)]
+    elf1 = [i for i in range(int(elf1[0]), int(elf1[1])+1)]
+
+    if all(list(map(lambda x: x in elf1, elf0))) or all(list(map(lambda x: x in elf0, elf1))): count += 1
+
+print(count)
+
